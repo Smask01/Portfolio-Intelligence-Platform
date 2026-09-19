c745236507aac487dd92b9115bf03f36b3d713f8
mughe <mughees.saeed@traildogsoftware.com>
2026-09-19 17:14:49 -0500

    Remove `TestAlpha` endpoint and unregister generic HTTP client

--- PortfolioIntelligencePlatform.Api/Program.cs
+++ PortfolioIntelligencePlatform.Api/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddScoped<EtfOverlapCalculator>();
 builder.Services.Configure<AlphaVantageOptions>(builder.Configuration.GetSection("AlphaVantage"));
 builder.Services.AddHttpClient<IEtfDataProvider, AlphaVantageEtfDataProvider>();
 builder.Services.AddHttpClient<IStockDataProvider, AlphaVantageStockDataProvider>();
-builder.Services.AddHttpClient();
 
 builder.Services.AddSingleton<AlphaVantageRateLimiter>();
 
